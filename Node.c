4a309a0f4c97ce91b2e694747b634f87f711e641
Avinash Nagarajan <avinash.nagarajan@mycit.ie>
2018-12-08 20:36:37 +0100

    Replaced Malloc and Calloc with sizeof and tuned hop count

--- Node.c
+++ Node.c
@@ -21,10 +21,10 @@ AUTOSTART_PROCESSES(&broadcast_process);
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-    struct broadcast_message *m;
+    struct broadcast_message m;
     m = packetbuf_dataptr(); 
 
-     printf("Broadcast: %d.%d\nHop Count: %d\nSequence Number: %d", from->u8[0], from->u8[1], m->hop, m->seqno);
+     printf("Hop Count: %d\nSequence Number: %d", m.hop, m.seqno);
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
@@ -42,7 +42,7 @@ PROCESS_BEGIN();
 
 broadcast_open(&broadcast, 140, &broadcast_call);
 
-struct broadcast_message *message = calloc(sizeof(struct broadcast_message), 1);
+//struct broadcast_message *message = calloc(sizeof(struct broadcast_message), 1);
 while(1) {
 
 /* Delay 5-10 seconds */
@@ -51,10 +51,10 @@ etimer_set(&et, CLOCK_SECOND * 5 + random_rand() % (CLOCK_SECOND * 4));
 PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
 
-packetbuf_copyfrom(&msg, sizeOf(struct broadcast_message));
+packetbuf_copyfrom(&msg, sizeof(struct broadcast_message));
 broadcast_send(&broadcast);
 printf("Broadcast message sent\n");
-broadcast_send(&broadcast)
+broadcast_send(&broadcast);
 }
 
 PROCESS_END();
