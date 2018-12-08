e0faf49d2916aa1667a48b2f2a961c40022945e7
Avinash Nagarajan <avinash.nagarajan@mycit.ie>
2018-12-08 21:19:43 +0100

    Created a status struct to keep track of the incoming hop count

--- Node.c
+++ Node.c
@@ -13,6 +13,9 @@ struct broadcast_message {
   int seqno; 
   int  hop;
 };
+
+struct broadcast_message status;
+
 /*---------------------------------------------------------------------------*/
 PROCESS(broadcast_process, "Node Process");
 AUTOSTART_PROCESSES(&broadcast_process);
@@ -21,10 +24,13 @@ AUTOSTART_PROCESSES(&broadcast_process);
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-    struct broadcast_message m;
+    struct broadcast_message *m;
     m = packetbuf_dataptr(); 
+    status.hop = m->hop;
+    status.seqno = m->seqno;
 
-     printf("Hop Count: %d\nSequence Number: %d", m.hop, m.seqno);
+     printf("Hop Count: %d\n Sequence Number: %d", m->hop, m->seqno);
+
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
@@ -50,10 +56,10 @@ etimer_set(&et, CLOCK_SECOND * 5 + random_rand() % (CLOCK_SECOND * 4));
 
 PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-
-packetbuf_copyfrom(&msg, sizeof(struct broadcast_message));
+status.hop = status.hop + 1;
+packetbuf_copyfrom(&status, sizeof(struct broadcast_message));
 broadcast_send(&broadcast);
-printf("Broadcast message sent\n");
+printf("Broadcast message sent from Node\n");
 broadcast_send(&broadcast);
 }
 
